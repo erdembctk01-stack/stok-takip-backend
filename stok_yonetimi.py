4ef66671fc8e1ea7af989e237bf7f18795677eed
erdembctk01-stack <erdembctk01@gmail.com>
2025-12-25 16:06:04 +0300

    Update stok_yonetimi.py

--- stok_yonetimi.py
+++ stok_yonetimi.py
@@ -5,8 +5,23 @@ def stok_listele(db):
     for i in items:
         i['_id'] = str(i['_id'])
         i['stock'] = int(i.get('stock', 0))
+        i['category'] = i.get('category', 'Genel')
+        i['code'] = i.get('code', '-')
     return items
 
-def genel_sil(db, col, id):
-    db[col].delete_one({"_id": ObjectId(id)})
+def stok_guncelle(db, id, miktar):
+    # miktar pozitifse artırır, negatifse azaltır
+    db.products.update_one({"_id": ObjectId(id)}, {"$inc": {"stock": miktar}})
+    return {"ok": True}
+
+def parca_ekle(db, data):
+    # Adet boşsa 1 kabul et
+    adet = int(data.get('stock')) if data.get('stock') else 1
+    db.products.insert_one({
+        "name": data['name'],
+        "code": data.get('code', '-'),
+        "category": data.get('category', 'Genel'),
+        "stock": adet,
+        "price": data.get('price', "0")
+    })
     return {"ok": True}
