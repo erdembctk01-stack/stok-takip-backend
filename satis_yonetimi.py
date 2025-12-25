c94136764610c01492a5b34f84b87d1133c62ea0
erdembctk01-stack <erdembctk01@gmail.com>
2025-12-25 21:58:16 +0300

    Update satis_yonetimi.py

--- satis_yonetimi.py
+++ satis_yonetimi.py
@@ -1,39 +1,33 @@
 from flask import Blueprint, request, jsonify
 from bson.objectid import ObjectId
 from datetime import datetime
+from app import db # Eski yöntemde db doğrudan app'den çekiliyordu
 
 satis_bp = Blueprint('satis_bp', __name__)
-db = None
-
-def init_db(db_instance):
-    global db
-    db = db_instance
 
 @satis_bp.route('/api/fatura-kes', methods=['POST'])
 def fatura_kes():
     data = request.json
+    # Eski mantık: Fiyatı veritabanından çekmeye çalışır veya limit koyar
     product = db.products.find_one({"_id": ObjectId(data['parcaId'])})
-    fiyat = float(str(data.get('fiyat', '0')).replace(',','.'))
+    
+    satis_fiyati = float(product.get('price', 0)) # Fiyatı stoktan alıyordu
     adet = int(data.get('adet', 1))
     
-    db.invoices.insert_one({
-        "ad": data.get('ad', 'Müşteri'),
-        "tel": data.get('tel', ''),
+    fatura = {
+        "ad": data['ad'],
         "parca_ad": product['name'],
         "adet": adet,
-        "toplam": fiyat * adet,
-        "tarih": datetime.now().strftime('%d.%m.%Y %H:%M')
-    })
-    db.products.update_one({"_id": ObjectId(data['parcaId'])}, {"$inc": {"stock": -adet}})
+        "toplam": satis_fiyati * adet,
+        "tarih": datetime.now().strftime('%d.%m.%Y')
+    }
+    
+    db.invoices.insert_one(fatura)
     return jsonify({"status": "success"})
 
 @satis_bp.route('/api/invoices', methods=['GET'])
-def get_inv():
-    inv = list(db.invoices.find().sort("_id", -1))
-    for i in inv: i['_id'] = str(i['_id'])
-    return jsonify(inv)
-
-@satis_bp.route('/api/invoices/<id>', methods=['DELETE'])
-def del_inv(id):
-    db.invoices.delete_one({"_id": ObjectId(id)})
-    return jsonify({"status": "success"})
+def get_invoices():
+    invoices = list(db.invoices.find())
+    for inv in invoices:
+        inv['_id'] = str(inv['_id'])
+    return jsonify(invoices)
