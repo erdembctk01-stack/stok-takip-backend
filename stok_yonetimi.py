14ca608554c33368970b467ca9741b91824a1d40
erdembctk01-stack <erdembctk01@gmail.com>
2025-12-25 16:19:52 +0300

    Update stok_yonetimi.py

--- stok_yonetimi.py
+++ stok_yonetimi.py
@@ -1,22 +1,11 @@
 from bson.objectid import ObjectId
 
-def stok_listele(db):
-    items = list(db.products.find())
-    for i in items:
-        i['_id'] = str(i['_id'])
-        i['stock'] = int(i.get('stock', 0))
-        i['category'] = i.get('category', 'Genel')
-        i['code'] = i.get('code', '-')
-    return items
-
 def stok_guncelle(db, id, miktar):
-    # miktar pozitifse artırır, negatifse azaltır
     db.products.update_one({"_id": ObjectId(id)}, {"$inc": {"stock": miktar}})
     return {"ok": True}
 
 def parca_ekle(db, data):
-    # Adet boşsa 1 kabul et
-    adet = int(data.get('stock')) if data.get('stock') else 1
+    adet = int(data.get('stock')) if data.get('stock') and str(data.get('stock')).strip() != "" else 1
     db.products.insert_one({
         "name": data['name'],
         "code": data.get('code', '-'),
