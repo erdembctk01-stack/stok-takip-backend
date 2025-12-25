c77e698fbe0f68e88d10067e9249c7a427dbe20e
erdembctk01-stack <erdembctk01@gmail.com>
2025-12-25 22:43:51 +0300

    Update satis_yonetimi.py

--- satis_yonetimi.py
+++ satis_yonetimi.py
@@ -10,8 +10,13 @@ def fatura_kes(db, data):
     # Stok Düş
     db.products.update_one({"_id": ObjectId(parca_id)}, {"$inc": {"stock": -adet}})
     
-    fiyat = float(str(parca.get('price', 0)).replace('.', '').replace(',', '.'))
-    toplam = fiyat * adet
+    # DÜZELTME: Fiyat artık stoktan değil, senin yazdığın 'fat-fiyat' alanından geliyor
+    try:
+        satis_fiyati = float(str(data.get('fiyat', 0)).replace('.', '').replace(',', '.'))
+    except:
+        satis_fiyati = 0.0
+        
+    toplam = satis_fiyati * adet
     
     fatura_verisi = {
         "ad": data['ad'],
@@ -19,10 +24,13 @@ def fatura_kes(db, data):
         "parca_ad": parca['name'],
         "adet": adet,
         "tarih": data['tarih'],
+        "birim_fiyat": satis_fiyati, # Analiz için birim fiyatı ekledik
         "toplam": toplam
     }
     
     db.invoices.insert_one(fatura_verisi)
+    
+    # Müşteriyi Cari Rehber'e ekle/güncelle
     db.customers.update_one(
         {"tel": data.get('tel')},
         {"$set": {"ad": data['ad'], "son_islem": data['tarih']}},
