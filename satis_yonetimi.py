419693a2636e12e8c3b69464294e89229fc17c38
erdembctk01-stack <erdembctk01@gmail.com>
2025-12-25 22:29:49 +0300

    Update satis_yonetimi.py

--- satis_yonetimi.py
+++ satis_yonetimi.py
@@ -1,43 +1,31 @@
-from flask import Blueprint, request, jsonify
 from bson.objectid import ObjectId
-from datetime import datetime
 
-satis_bp = Blueprint('satis_bp', __name__)
-db = None
-
-def init_db(database_instance):
-    global db
-    db = database_instance
-
-@satis_bp.route('/api/fatura-kes', methods=['POST'])
-def fatura_kes():
-    data = request.json
-    product = db.products.find_one({"_id": ObjectId(data['parcaId'])})
-    
-    # Eski mantık: Fiyatı sadece stoktan çeker
-    satis_fiyati = float(product.get('price', 0))
+def fatura_kes(db, data):
+    parca_id = data.get('parcaId')
     adet = int(data.get('adet', 1))
+    parca = db.products.find_one({"_id": ObjectId(parca_id)})
     
-    fatura = {
+    if not parca: return {"ok": False}
+    
+    # Stok Düş
+    db.products.update_one({"_id": ObjectId(parca_id)}, {"$inc": {"stock": -adet}})
+    
+    fiyat = float(str(parca.get('price', 0)).replace('.', '').replace(',', '.'))
+    toplam = fiyat * adet
+    
+    fatura_verisi = {
         "ad": data['ad'],
-        "parca_ad": product['name'],
+        "tel": data.get('tel', '-'),
+        "parca_ad": parca['name'],
         "adet": adet,
-        "toplam": satis_fiyati * adet,
-        "tarih": datetime.now().strftime('%d.%m.%Y')
+        "tarih": data['tarih'],
+        "toplam": toplam
     }
     
-    db.invoices.insert_one(fatura)
-    db.products.update_one({"_id": ObjectId(data['parcaId'])}, {"$inc": {"stock": -adet}})
-    return jsonify({"status": "success"})
-
-@satis_bp.route('/api/invoices', methods=['GET'])
-def get_invoices():
-    invoices = list(db.invoices.find())
-    for inv in invoices:
-        inv['_id'] = str(inv['_id'])
-    return jsonify(invoices)
-
-@satis_bp.route('/api/invoices/<id>', methods=['DELETE'])
-def delete_invoice(id):
-    db.invoices.delete_one({"_id": ObjectId(id)})
-    return jsonify({"status": "success"})
+    db.invoices.insert_one(fatura_verisi)
+    db.customers.update_one(
+        {"tel": data.get('tel')},
+        {"$set": {"ad": data['ad'], "son_islem": data['tarih']}},
+        upsert=True
+    )
+    return {"ok": True}
