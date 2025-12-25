366f9baf5268d9cb2a592f86efbd5e95dc7d7fee
erdembctk01-stack <erdembctk01@gmail.com>
2025-12-25 15:02:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
+import os
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 from pymongo import MongoClient
-import stok_yonetimi  # Diğer dosyayı çağırır
-import satis_yonetimi # Diğer dosyayı çağırır
+import stok_yonetimi 
+import satis_yonetimi
 
 app = Flask(__name__)
 CORS(app)
 
+# MongoDB Bağlantısı
 MONGO_URI = "mongodb+srv://erdembctk01_db_user:Dyta96252@cluster0.o27rfmv.mongodb.net/stok_veritabani?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(MONGO_URI)
 db = client.stok_veritabani
 
 @app.route('/')
 def index():
+    # Burası beyaz ekranı önleyen kısımdır. templates/index.html'i çağırır.
     return render_template('index.html')
 
 @app.route('/api/products', methods=['GET'])
@@ -28,4 +31,5 @@ def post_fatura():
     return jsonify(satis_yonetimi.fatura_kes(db, request.json))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
