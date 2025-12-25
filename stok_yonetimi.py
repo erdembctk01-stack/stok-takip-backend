064f05a9e94bc1087b3a0a1ec99999e6a572dd09
erdembctk01-stack <erdembctk01@gmail.com>
2025-12-25 15:51:12 +0300

    Update stok_yonetimi.py

--- stok_yonetimi.py
+++ stok_yonetimi.py
@@ -4,10 +4,9 @@ def stok_listele(db):
     items = list(db.products.find())
     for i in items:
         i['_id'] = str(i['_id'])
-        i['stock'] = int(i.get('stock', 0)) # Sayısal hata koruması
-        i['price'] = i.get('price', "0")
+        i['stock'] = int(i.get('stock', 0))
     return items
 
-def stok_sil(db, id):
-    db.products.delete_one({"_id": ObjectId(id)})
+def genel_sil(db, col, id):
+    db[col].delete_one({"_id": ObjectId(id)})
     return {"ok": True}
