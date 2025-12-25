dca896ec442f2a44af4a1c6d0eafaeb87f122215
erdembctk01-stack <erdembctk01@gmail.com>
2025-12-25 21:58:36 +0300

    Update stok_yonetimi.py

--- stok_yonetimi.py
+++ stok_yonetimi.py
@@ -1,34 +1,27 @@
 from flask import Blueprint, request, jsonify
 from bson.objectid import ObjectId
+from app import db
 
 stok_bp = Blueprint('stok_bp', __name__)
-db = None
-
-def init_db(db_instance):
-    global db
-    db = db_instance
 
 @stok_bp.route('/api/products', methods=['GET', 'POST'])
-def products():
+def manage_products():
     if request.method == 'POST':
+        data = request.json
         db.products.insert_one({
-            "name": request.json['name'],
-            "code": request.json['code'],
-            "category": request.json.get('category', 'Genel'),
-            "price": request.json.get('price', '0'),
-            "stock": int(request.json.get('stock', 0))
+            "name": data['name'],
+            "code": data['code'],
+            "price": data['price'],
+            "stock": int(data['stock'])
         })
         return jsonify({"status": "success"})
-    p_list = list(db.products.find())
-    for p in p_list: p['_id'] = str(p['_id'])
-    return jsonify(p_list)
-
-@stok_bp.route('/api/products/update/<id>', methods=['POST'])
-def update_p(id):
-    db.products.update_one({"_id": ObjectId(id)}, {"$inc": {"stock": int(request.json['miktar'])}})
-    return jsonify({"status": "success"})
+    
+    products = list(db.products.find())
+    for p in products:
+        p['_id'] = str(p['_id'])
+    return jsonify(products)
 
 @stok_bp.route('/api/products/<id>', methods=['DELETE'])
-def del_p(id):
+def delete_product(id):
     db.products.delete_one({"_id": ObjectId(id)})
     return jsonify({"status": "success"})
