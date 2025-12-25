c39e2d24b992bda5da3f3c63aa911983bb837b7c
erdembctk01-stack <erdembctk01@gmail.com>
2025-12-25 16:06:18 +0300

    Update satis_yonetimi.py

--- satis_yonetimi.py
+++ satis_yonetimi.py
@@ -1,24 +1,29 @@
 from bson.objectid import ObjectId
 
 def fatura_kes(db, data):
-    try:
-        parca_id = data.get('parcaId')
-        adet = int(data.get('adet', 0))
-        parca = db.products.find_one({"_id": ObjectId(parca_id)})
-        
-        # Stoktan Düşme
-        yeni_stok = int(parca.get('stock', 0)) - adet
-        db.products.update_one({"_id": ObjectId(parca_id)}, {"$set": {"stock": yeni_stok}})
-        
-        # Fatura Kaydı (Tüm detaylarıyla)
-        db.invoices.insert_one({
-            "ad": data['ad'],
-            "tel": data.get('tel', ''),
-            "parca_ad": parca['name'],
-            "adet": adet,
-            "tarih": data['tarih'],
-            "fiyat": parca.get('price', 0)
-        })
-        return {"ok": True}
-    except Exception as e:
-        return {"ok": False, "error": str(e)}
+    parca_id = data.get('parcaId')
+    adet = int(data.get('adet', 1))
+    parca = db.products.find_one({"_id": ObjectId(parca_id)})
+    
+    # Stok Düş
+    db.products.update_one({"_id": ObjectId(parca_id)}, {"$inc": {"stock": -adet}})
+    
+    fatura_verisi = {
+        "musteri": data['ad'],
+        "tel": data.get('tel', '-'),
+        "parca_ad": parca['name'],
+        "adet": adet,
+        "tarih": data['tarih'],
+        "toplam": float(parca.get('price', 0).replace(',','.')) * adet
+    }
+    
+    # Loglara kaydet
+    db.invoices.insert_one(fatura_verisi)
+    
+    # Cari Rehbere kaydet (Eğer müşteri yoksa ekle)
+    db.customers.update_one(
+        {"tel": data.get('tel')},
+        {"$set": {"ad": data['ad'], "son_islem": data['tarih']}},
+        upsert=True
+    )
+    return {"ok": True}
