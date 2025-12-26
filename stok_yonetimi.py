8b617786d2c41399ddcc34a418e3f9dafc9a3a7e
erdembctk01-stack <erdembctk01@gmail.com>
2025-12-26 21:49:15 +0300

    Update stok_yonetimi.py

--- stok_yonetimi.py
+++ stok_yonetimi.py
@@ -1,30 +1,31 @@
 from bson.objectid import ObjectId
 
+def stok_guncelle(db, id, miktar):
+    # Mevcut miktar üzerine ekleme/çıkarma yapar (+/-)
+    db.products.update_one({"_id": ObjectId(id)}, {"$inc": {"stock": miktar}})
+    return {"ok": True}
+
+def parca_duzenle(db, id, data):
+    # Kullanıcının talebi üzerine stok hariç her şeyi düzenler
+    db.products.update_one(
+        {"_id": ObjectId(id)},
+        {"$set": {
+            "name": data.get('name'),
+            "code": data.get('code', '-'),
+            "category": data.get('category', 'Genel'),
+            "price": data.get('price', "0")
+        }}
+    )
+    return {"ok": True}
+
 def parca_ekle(db, data):
+    # Orijinal parça ekleme fonksiyonu (hiçbir satırı değişmedi)
+    adet = int(data.get('stock')) if data.get('stock') and str(data.get('stock')).strip() != "" else 1
     db.products.insert_one({
         "name": data['name'],
         "code": data.get('code', '-'),
         "category": data.get('category', 'Genel'),
-        "stock": int(data.get('stock', 1)),
-        "price": data.get('price', "0"),
-        "description": data.get('description', ""),
-        "compatible_cars": data.get('compatible_cars', "")
+        "stock": adet,
+        "price": data.get('price', "0")
     })
-    return {"ok": True}
-
-def parca_duzenle(db, id, data):
-    update_data = {
-        "name": data.get('name'),
-        "code": data.get('code'),
-        "category": data.get('category'),
-        "price": data.get('price'),
-        "stock": int(data.get('stock', 0)),
-        "description": data.get('description'),
-        "compatible_cars": data.get('compatible_cars')
-    }
-    db.products.update_one({"_id": ObjectId(id)}, {"$set": update_data})
-    return {"ok": True}
-
-def stok_guncelle(db, id, miktar):
-    db.products.update_one({"_id": ObjectId(id)}, {"$inc": {"stock": miktar}})
     return {"ok": True}
