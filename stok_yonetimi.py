d204958694ed8f843451a018f40c0d4865a929a8
erdembctk01-stack <erdembctk01@gmail.com>
2025-12-26 21:56:29 +0300

    Update stok_yonetimi.py

--- stok_yonetimi.py
+++ stok_yonetimi.py
@@ -1,31 +1,26 @@
 from bson.objectid import ObjectId
 
 def stok_guncelle(db, id, miktar):
-    # Mevcut miktar üzerine ekleme/çıkarma yapar (+/-)
     db.products.update_one({"_id": ObjectId(id)}, {"$inc": {"stock": miktar}})
     return {"ok": True}
 
 def parca_duzenle(db, id, data):
-    # Kullanıcının talebi üzerine stok hariç her şeyi düzenler
-    db.products.update_one(
-        {"_id": ObjectId(id)},
-        {"$set": {
-            "name": data.get('name'),
-            "code": data.get('code', '-'),
-            "category": data.get('category', 'Genel'),
-            "price": data.get('price', "0")
-        }}
-    )
+    # Stok hariç güncelleme
+    update_data = {
+        "name": data.get('name'),
+        "code": data.get('code'),
+        "category": data.get('category'),
+        "price": data.get('price')
+    }
+    db.products.update_one({"_id": ObjectId(id)}, {"$set": update_data})
     return {"ok": True}
 
 def parca_ekle(db, data):
-    # Orijinal parça ekleme fonksiyonu (hiçbir satırı değişmedi)
-    adet = int(data.get('stock')) if data.get('stock') and str(data.get('stock')).strip() != "" else 1
     db.products.insert_one({
         "name": data['name'],
         "code": data.get('code', '-'),
         "category": data.get('category', 'Genel'),
-        "stock": adet,
+        "stock": int(data.get('stock', 1)),
         "price": data.get('price', "0")
     })
     return {"ok": True}
