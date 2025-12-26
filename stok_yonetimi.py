413d4415e1a6283540e40e7dff6a2f03b43be63f
erdembctk01-stack <erdembctk01@gmail.com>
2025-12-26 22:27:12 +0300

    Update stok_yonetimi.py

--- stok_yonetimi.py
+++ stok_yonetimi.py
@@ -5,12 +5,13 @@ def stok_guncelle(db, id, miktar):
     return {"ok": True}
 
 def parca_duzenle(db, id, data):
-    # Stok hariç güncelleme
+    # FİYAT HARİÇ her şeyi günceller
     update_data = {
         "name": data.get('name'),
         "code": data.get('code'),
         "category": data.get('category'),
-        "price": data.get('price')
+        "desc": data.get('desc', ''),        # Yeni: Açıklama
+        "compat": data.get('compat', '')     # Yeni: Uyumlu Araç
     }
     db.products.update_one({"_id": ObjectId(id)}, {"$set": update_data})
     return {"ok": True}
@@ -20,6 +21,8 @@ def parca_ekle(db, data):
         "name": data['name'],
         "code": data.get('code', '-'),
         "category": data.get('category', 'Genel'),
+        "desc": data.get('desc', ''),        # Yeni: Açıklama
+        "compat": data.get('compat', ''),    # Yeni: Uyumlu Araç
         "stock": int(data.get('stock', 1)),
         "price": data.get('price', "0")
     })
