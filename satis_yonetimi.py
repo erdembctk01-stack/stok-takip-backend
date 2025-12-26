a8e65d0bf281d9467cbfabcfc5201e2fe9161c54
erdembctk01-stack <erdembctk01@gmail.com>
2025-12-26 21:31:01 +0300

    Update satis_yonetimi.py

--- satis_yonetimi.py
+++ satis_yonetimi.py
@@ -1,34 +1,36 @@
 from bson.objectid import ObjectId
 
+def toplu_fatura_kes(db, data):
+    ad = data.get('ad')
+    tel = data.get('tel', '-')
+    items = data.get('items', [])
+    tarih = data.get('tarih')
+    
+    toplam_tutar = 0
+    parca_isimleri = []
+
+    for item in items:
+        p_id = item['parcaId']
+        adet = int(item['adet'])
+        fiyat = float(str(item['fiyat']).replace(',','.'))
+        db.products.update_one({"_id": ObjectId(p_id)}, {"$inc": {"stock": -adet}})
+        toplam_tutar += (adet * fiyat)
+        parca_isimleri.append(f"{item['name']} (x{adet})")
+
+    db.invoices.insert_one({
+        "ad": ad, "tel": tel, "parca_ad": ", ".join(parca_isimleri),
+        "tarih": tarih, "toplam": toplam_tutar
+    })
+    
+    db.customers.update_one({"tel": tel}, {"$set": {"ad": ad, "son_islem": tarih}}, upsert=True)
+    return {"ok": True}
+
 def fatura_kes(db, data):
-    parca_id = data.get('parcaId')
-    adet = int(data.get('adet', 1))
-    parca = db.products.find_one({"_id": ObjectId(parca_id)})
-    
-    if not parca: return {"ok": False}
-    
-    db.products.update_one({"_id": ObjectId(parca_id)}, {"$inc": {"stock": -adet}})
-    
-    try:
-        satis_fiyati = float(str(data.get('fiyat', 0)).replace(',', '.'))
-    except:
-        satis_fiyati = 0.0
-    
-    toplam = satis_fiyati * adet
-    
-    fatura_verisi = {
-        "ad": data['ad'],
-        "tel": data.get('tel', '-'),
-        "parca_ad": parca['name'],
-        "adet": adet,
-        "tarih": data['tarih'],
-        "toplam": toplam
-    }
-    
-    db.invoices.insert_one(fatura_verisi)
-    db.customers.update_one(
-        {"tel": data.get('tel')},
-        {"$set": {"ad": data['ad'], "son_islem": data['tarih']}},
-        upsert=True
-    )
+    # Tekli satış fonksiyonu (Geriye dönük uyumluluk için duruyor)
+    parca = db.products.find_one({"_id": ObjectId(data['parcaId'])})
+    db.products.update_one({"_id": ObjectId(data['parcaId'])}, {"$inc": {"stock": -int(data['adet'])}})
+    db.invoices.insert_one({
+        "ad": data['ad'], "tel": data['tel'], "parca_ad": parca['name'],
+        "tarih": data['tarih'], "toplam": float(str(data['fiyat']).replace(',','.')) * int(data['adet'])
+    })
     return {"ok": True}
