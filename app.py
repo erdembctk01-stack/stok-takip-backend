7107d57eec4154fd4a45decde376234ae96a2990
erdembctk01-stack <erdembctk01@gmail.com>
2025-12-27 02:22:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -35,6 +35,10 @@ def handle_generic_api(col):
 def update_stock(id):
     miktar = request.json.get('miktar', 0)
     return jsonify(stok_yonetimi.stok_guncelle(db, id, miktar))
+    
+    @app.route('/api/products/edit/<id>', methods=['POST'])
+def edit_product(id):
+    return jsonify(stok_yonetimi.parca_duzenle(db, id, request.json))
 
 @app.route('/api/products/edit/<id>', methods=['POST'])
 def edit_product(id):
